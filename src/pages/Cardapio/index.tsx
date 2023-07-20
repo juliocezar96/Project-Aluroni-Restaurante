31fac7d6b9de7a68a2dcecc3afb71894ab2c8879
Julio Cezar <julio.cezar@liferay.com>
2023-07-20 16:53:22 -0300

    This reverts commit 5632294766d4883abebc39855195d9ee873f60da.
    	modified:   src/pages/Cardapio/index.tsx

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -10,9 +10,6 @@ export default function Cardapio() {
   const [filtro, setFiltro] = useState<number | null>(null);
   const [ordenador, setOrdenador] = useState("");
 
-
-  
-
   return (
     <main>
       <nav className={styles.menu}>
