5b7086d3a2cc917ff6d1e44087c07fd06aba1c53
Julio Cezar <julio.cezar@liferay.com>
2023-08-02 13:42:40 -0300

    Implementation of Router Dom Hook, NotFound, Footer

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,27 @@
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cardapio from 'pages/Cardapio';
-import Inicio from 'pages/Cardapio/Inicio';
+import Inicio from 'pages/Inicio';
 import Menu from 'components/Menu';
 import PaginaPadrao from 'components/PaginaPadrao';
+import Sobre from 'pages/Sobre';
+import Footer from 'components/Footer';
+import NotFound from 'pages/NotFound';
 
 
 export default function AppRouter() {
   return(
-    <main>
+    <main className='container'>
       <Router>
         <Menu />
         <Routes>
           <Route path='/' element={<PaginaPadrao />} >
             <Route index element={<Inicio />} />
             <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
           </Route>
-        
+          <Route path='*' element={<NotFound />} />
         </Routes> 
+        <Footer />
       </Router>
     </main>
   );
